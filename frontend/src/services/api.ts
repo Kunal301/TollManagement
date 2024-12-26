ef1b1572ccd6ddc966dd6d15fa1a41342bda2937
Kunal Soni <kunalsoni20001@gmail.com>
2024-12-26 18:07:59 +0530

    other changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,9 @@
 import axios,{ AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://tollmanagement-a7vi.onrender.com';
 
 const api: AxiosInstance = axios.create({
-  baseURL: API_BASE_URL || 'http://localhost:5000',
+  baseURL: API_BASE_URL || 'https://tollmanagement-a7vi.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
